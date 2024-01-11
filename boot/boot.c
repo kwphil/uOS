d6d981221d0987e56cead42698a70ab5c9a5d0e5
kwphil <121273949+kwphil@users.noreply.github.com>
2024-01-11 11:30:06 -0600

    Update boot.c

--- boot/boot.c
+++ boot/boot.c
@@ -1,5 +1,10 @@
 /*****************************
  * uOS Boot software         *
+ *                           *
+ * For simplicity, boot will *
+ * use VGA to write to the   *
+ * screen                    *
+ *                           *
  * Memory map:               *
  *     0x0 + ------------ +  *
  *         |  bootloader  |  *
@@ -10,4 +15,21 @@
  *         |              |  *
  *         |              |  *
  * 0x30000 + ------------ +  *
+ *                           *
+ * OS created by: kwphil     *
  *****************************/
+
+#define VGA_BUF 0xb8000
+
+int _start() {
+    *(char *)VGA_BUF = "strt bt";
+
+    
+}
+
+int vga_print(char *input, ) {
+    register curr_offset;
+    __asm__("addi %0,0,[1]");
+    
+    for
+}
